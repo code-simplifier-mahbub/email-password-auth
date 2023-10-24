bb910bb78f888a4fe9c6383b777a66201bb165bd
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2023-10-24 20:52:21 +0600

    create sign in with email password

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDEhHgJ_kQcKK_n9qXgGs-0qMU-J-rJHD0",
+  authDomain: "email-password-auth-2389b.firebaseapp.com",
+  projectId: "email-password-auth-2389b",
+  storageBucket: "email-password-auth-2389b.appspot.com",
+  messagingSenderId: "1058455725147",
+  appId: "1:1058455725147:web:042bc25da10565e2c00b7e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
