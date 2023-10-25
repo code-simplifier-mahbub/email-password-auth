c0cbc1b007bac862874253786cd3dddb8f5d9b36
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2023-10-25 17:13:15 +0600

    [DONE] get form email and password

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 
+const handleRegister = (event) => {
+  event.preventDefault();
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  console.log(email, password)
+}
+
+const handleChange= (event) =>{
+  console.log(event.target.value)
+}
+
 function App() {
   return (
+  
     <div className="App">
-      <form >
-        <input type="email" name="" id="" placeholder='Your email' />
-        <input type="password" name="" id="" placeholder='Your password' />
+      <form onSubmit={handleRegister}>
+        <input onChange={handleChange} type="email" name="email" id="" placeholder='Your email'/>
+        <input type="password" name="password" id="" placeholder='Your password' />
+        <br />
+        <button type='submit'>Register</button>
       </form>
     </div>
   );
